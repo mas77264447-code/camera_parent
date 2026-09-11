4b202bf918f931ff55f5cbc6671eefc7e6008b1a
mas77264447-code <mas77264447@gmail.com>
2026-09-11 14:11:00 -0700

    Update FlutterServiceBridge.kt

--- android/app/src/main/kotlin/com/example/camera_parent/FlutterServiceBridge.kt
+++ android/app/src/main/kotlin/com/example/camera_parent/FlutterServiceBridge.kt
@@ -1,13 +1,68 @@
 package com.example.camera_parent
 
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.dart.DartExecutor
 import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 object FlutterServiceBridge {
+
     private const val CHANNEL = "camera_parent/service"
 
+
     fun startAgent() {
-        val app = CameraParentApplication.AppHolder.engine ?: return
-        MethodChannel(app.dartExecutor.binaryMessenger, CHANNEL)
-            .invokeMethod("startAgent", null)
+
+        val engine = getOrCreateEngine()
+
+        MethodChannel(
+            engine.dartExecutor.binaryMessenger,
+            CHANNEL
+        ).invokeMethod(
+            "startAgent",
+            null
+        )
+    }
+
+
+    private fun getOrCreateEngine(): FlutterEngine {
+
+        // استخدام FlutterEngine الموجود
+        CameraParentApplication.AppHolder.engine?.let {
+            return it
+        }
+
+
+        // إنشاء Engine جديد إذا تم قتله بعد ضغط X
+        val context =
+            CameraParentApplication.AppHolder.context
+                ?: throw IllegalStateException(
+                    "Application context missing"
+                )
+
+
+        val engine = FlutterEngine(context)
+
+
+        GeneratedPluginRegistrant.registerWith(engine)
+
+
+        engine.dartExecutor.executeDartEntrypoint(
+            DartExecutor.DartEntrypoint.createDefault()
+        )
+
+
+        CameraParentApplication.AppHolder.engine = engine
+
+
+        FlutterEngineCache
+            .getInstance()
+            .put(
+                CameraParentApplication.ENGINE_ID,
+                engine
+            )
+
+
+        return engine
     }
 }
